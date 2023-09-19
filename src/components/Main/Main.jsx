4d7d6d45713e0884b69729b9110a94b849681be2
cristiantrilleras <cris_01lds@yahoo.es>
2023-09-19 09:43:42 -0500

    cambion en section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { SectionImage } from '../SectionImage/SectionImage'
-import { Sectiontext } from '../SectionText/Sectiontext'
+import Sectiontext from '../Sectiontext/Sectiontext'
 import './Main.css';
 
 export const Main = () => {
   return (
     <main className='Mainp'>
       <Sectiontext/>
+
 
       <section className='contenedorimg'>
       <SectionImage/>
