d1f60f72440b44ccc178e9205733168ecec0e7b6
mauro <andrekxus299@gmail.com>
2023-10-02 15:58:57 -0500

    agregue imagen local

--- src/components/SectionImage/SectionImage.jsx
+++ src/components/SectionImage/SectionImage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import './image.css'
+import miImagen from '../../assets/cristian.jpg'
 
 export const SectionImage = () => {
   return (
-    <img src="https://scontent.fclo10-1.fna.fbcdn.net/v/t39.30808-6/335448009_247966160914752_923571847570643412_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=a2f6c7&_nc_ohc=MbB3LCZFDI0AX9kfN35&_nc_zt=23&_nc_ht=scontent.fclo10-1.fna&oh=00_AfABbz-7QoUlLQf44QkGiZqoL0HE56MtDVlxH1RkaAhs4Q&oe=650E5336" alt=""  className='Image'/>
+
+
+    <img src={miImagen} alt="Descripción de la imagen" className='Image'/>
+
+    
   )
 }
